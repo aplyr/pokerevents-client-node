bca247255166ef6d86a42fca466596311c9ced24
Stefán Kornél <kornel.stefan@ingenimind.com>
2024-05-09 11:36:44 +0200

    Generated new version of client

--- api/embedded-api.ts
+++ api/embedded-api.ts
@@ -26,6 +26,8 @@ import { EventDetailsDto } from '../models';
 // @ts-ignore
 import { LiveReportingDetailsDto } from '../models';
 // @ts-ignore
+import { LiveUpdateDto } from '../models';
+// @ts-ignore
 import { ProblemDetails } from '../models';
 // @ts-ignore
 import { Result } from '../models';
@@ -107,6 +109,47 @@ export const EmbeddedApiAxiosParamCreator = function (configuration?: Configurat
                 options: localVarRequestOptions,
             };
         },
+        /**
+         * 
+         * @param {string} liveReportingId 
+         * @param {number} [page] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        livereportingsLiveReportingIdUpdatesGet: async (liveReportingId: string, page?: number, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'liveReportingId' is not null or undefined
+            assertParamExists('livereportingsLiveReportingIdUpdatesGet', 'liveReportingId', liveReportingId)
+            const localVarPath = `/livereportings/{liveReportingId}/updates`
+                .replace(`{${"liveReportingId"}}`, encodeURIComponent(String(liveReportingId)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication bearer required
+            await setApiKeyToObject(localVarQueryParameter, "Authorization", configuration)
+
+            if (page !== undefined) {
+                localVarQueryParameter['page'] = page;
+            }
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
     }
 };
 
@@ -141,6 +184,19 @@ export const EmbeddedApiFp = function(configuration?: Configuration) {
             const localVarOperationServerBasePath = operationServerMap['EmbeddedApi.apiEmbeddedLivereportingsLiveReportingIdGet']?.[localVarOperationServerIndex]?.url;
             return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
         },
+        /**
+         * 
+         * @param {string} liveReportingId 
+         * @param {number} [page] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async livereportingsLiveReportingIdUpdatesGet(liveReportingId: string, page?: number, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<Array<LiveUpdateDto>>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.livereportingsLiveReportingIdUpdatesGet(liveReportingId, page, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['EmbeddedApi.livereportingsLiveReportingIdUpdatesGet']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
     }
 };
 
@@ -168,6 +224,16 @@ export const EmbeddedApiFactory = function (configuration?: Configuration, baseP
          */
         apiEmbeddedLivereportingsLiveReportingIdGet(liveReportingId: string, options?: any): AxiosPromise<LiveReportingDetailsDto> {
             return localVarFp.apiEmbeddedLivereportingsLiveReportingIdGet(liveReportingId, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
+         * @param {string} liveReportingId 
+         * @param {number} [page] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        livereportingsLiveReportingIdUpdatesGet(liveReportingId: string, page?: number, options?: any): AxiosPromise<Array<LiveUpdateDto>> {
+            return localVarFp.livereportingsLiveReportingIdUpdatesGet(liveReportingId, page, options).then((request) => request(axios, basePath));
         },
     };
 };
@@ -199,6 +265,18 @@ export class EmbeddedApi extends BaseAPI {
      */
     public apiEmbeddedLivereportingsLiveReportingIdGet(liveReportingId: string, options?: RawAxiosRequestConfig) {
         return EmbeddedApiFp(this.configuration).apiEmbeddedLivereportingsLiveReportingIdGet(liveReportingId, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @param {string} liveReportingId 
+     * @param {number} [page] 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof EmbeddedApi
+     */
+    public livereportingsLiveReportingIdUpdatesGet(liveReportingId: string, page?: number, options?: RawAxiosRequestConfig) {
+        return EmbeddedApiFp(this.configuration).livereportingsLiveReportingIdUpdatesGet(liveReportingId, page, options).then((request) => request(this.axios, this.basePath));
     }
 }
 
